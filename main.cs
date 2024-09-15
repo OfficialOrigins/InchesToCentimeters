df22ff8a62316d4e4658ec502c2c13f13cea33aa
OfficialOrigins <138532627+OfficialOrigins@users.noreply.github.com>
2024-09-15 22:14:38 +0000

    New program added (2.1.1) and commented old one

--- main.cs
+++ main.cs
@@ -1,3 +1,44 @@
+//Module 2 excercise 2.1 
+// using System;
+
+// class Program 
+// {
+//     public static void Main (string[] args) 
+//     {
+//       double finalResult; 
+//       const double InchToCm = 2.54;
+//       int inch = 5;
+
+//       finalResult = inch * InchToCm;
+
+//       Console.WriteLine("The amount of inches: {0}", inch);
+//       Console.WriteLine("Conversion to centimeters: {0}", finalResult);
+//     }
+// }
+
+//Module 2 excercise 2.1.1
+// using System;
+
+// class Program 
+// {
+//     public static void Main (string[] args) 
+//     {
+//       double finalResult; 
+//       const double Conversion = 2.54; 
+      
+//       Console.WriteLine("Enter the amount of inches: "); 
+//       int inch = Convert.ToInt32(Console.ReadLine());
+      
+//       Console.WriteLine(" ");
+      
+//       finalResult = inch * Conversion; 
+  
+//       Console.WriteLine("The amount of inches: {0}", inch);
+//       Console.WriteLine("Conversion to centimeters: {0}", finalResult);
+//     }
+// }
+
+//Module 2 excercise 2.1.2
 using System;
 
 class Program 
@@ -5,10 +46,14 @@ class Program
     public static void Main (string[] args) 
     {
       double finalResult; 
-      const double InchToCm = 2.54;
-      int inch = 5;
+      const double Conversion = 2.54; 
 
-      finalResult = inch * InchToCm;
+      Console.WriteLine("Enter the amount of inches: "); 
+      int inch = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine(" ");
+
+      finalResult = inch * Conversion; 
 
       Console.WriteLine("The amount of inches: {0}", inch);
       Console.WriteLine("Conversion to centimeters: {0}", finalResult);
